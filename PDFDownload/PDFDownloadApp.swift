d13fa3782ffe6123ea8593cc34ec645e3ae07f9c
Azharuddin <iamazharuddin10@gmail.com>
2023-05-06 11:08:40 +0530

    Initial Commit

---
+++ PDFDownload/PDFDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFDownloadApp.swift
+//  PDFDownload
+//
+//  Created by Azharuddin 1 on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PDFDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
